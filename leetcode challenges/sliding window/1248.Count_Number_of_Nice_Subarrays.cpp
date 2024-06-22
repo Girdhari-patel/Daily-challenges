bf452c79cf059a4c84e3ca45b35f74694d92c59d
Girdhari-patel <girdharip45@gmail.com>
2024-06-23 02:08:44 +0530

    sliding windowimp concept also done in hashmap

---
+++ leetcode challenges/sliding window/1248.Count_Number_of_Nice_Subarrays.cpp
@@ -0,0 +1,59 @@
+/*Given an array of integers nums and an integer k. A continuous subarray is called nice if there are k odd numbers on it.
+
+Return the number of nice sub-arrays.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,2,1,1], k = 3
+Output: 2
+Explanation: The only sub-arrays with 3 odd numbers are [1,1,2,1] and [1,2,1,1].
+Example 2:
+
+Input: nums = [2,4,6], k = 1
+Output: 0
+Explanation: There are no odd numbers in the array.
+Example 3:
+
+Input: nums = [2,2,2,1,2,2,1,2,2,2], k = 2
+Output: 16
+ 
+
+Constraints:
+
+1 <= nums.length <= 50000
+1 <= nums[i] <= 10^5
+1 <= k <= nums.length
+
+{2, 1, 2};
+*/
+// sliding window 
+// time complexity is O(n) becouse every element we see at once;
+class Solution {
+public:
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        int n = nums.size();
+        int oddCount=0;
+        int prevCount =0;
+        int result =0;
+        int i=0,j=0;
+
+        while(j<n){
+            if(nums[j]%2!=0){ // odd
+              oddCount++;
+              prevCount=0;
+            }
+            while(oddCount==k){
+                prevCount++;
+                if(i<n && nums[i]%2==1){
+                    oddCount--;
+                }
+                i++;
+            }
+            result+=prevCount;
+            j++;
+        }
+        return result;
+    }
+};
