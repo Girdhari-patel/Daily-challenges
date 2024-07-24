5543eec8a70e1e7b7bbf3453ab12094427fe0f24
Girdhari-patel <girdharip45@gmail.com>
2024-07-24 19:21:19 +0530

    please revise

---
+++ leetcode challenges/stack/1717.Maximum_Score_From_Removing_Substrings.cpp
@@ -0,0 +1,71 @@
+/*
+You are given a string s and two integers x and y. You can perform two types of operations any number of times.
+
+Remove substring "ab" and gain x points.
+For example, when removing "ab" from "cabxbae" it becomes "cxbae".
+Remove substring "ba" and gain y points.
+For example, when removing "ba" from "cabxbae" it becomes "cabxe".
+Return the maximum points you can gain after applying the above operations on s.
+
+ 
+
+Example 1:
+
+Input: s = "cdbcbbaaabab", x = 4, y = 5
+Output: 19
+Explanation:
+- Remove the "ba" underlined in "cdbcbbaaabab". Now, s = "cdbcbbaaab" and 5 points are added to the score.
+- Remove the "ab" underlined in "cdbcbbaaab". Now, s = "cdbcbbaa" and 4 points are added to the score.
+- Remove the "ba" underlined in "cdbcbbaa". Now, s = "cdbcba" and 5 points are added to the score.
+- Remove the "ba" underlined in "cdbcba". Now, s = "cdbc" and 5 points are added to the score.
+Total score = 5 + 4 + 5 + 5 = 19.
+Example 2:
+
+Input: s = "aabbaaxybbaabb", x = 5, y = 4
+Output: 20
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+1 <= x, y <= 104
+s consists of lowercase English letters.
+*/
+
+class Solution {
+public:
+   int pointsForStirng(string &s, string maksadString, int points){
+    int totalPoints = 0 , n = s.length();
+    stack<int>st;
+
+    for(int i=0;i<n;i++){
+        if(st.empty()){
+            st.push(s[i]);
+        }else{
+            if(s[i]==maksadString[1] && st.top()==maksadString[0]){
+                st.pop();
+                totalPoints+=points;
+            }else{
+                st.push(s[i]);
+            }
+        }
+    }
+
+    s = "";
+    while(!st.empty()){
+        // s.push_back(st.top());
+        s+=st.top();
+        st.pop();
+        }
+    reverse(s.begin(), s.end());
+    return totalPoints;
+   }
+
+     
+    int maximumGain(string s, int x, int y) {
+        string s1 ="ab", s2 = "ba";
+        if(x<y)swap(s1,s2), swap(x,y);
+
+        return pointsForStirng(s,s1,x)+pointsForStirng(s, s2, y);
+    }
+};
