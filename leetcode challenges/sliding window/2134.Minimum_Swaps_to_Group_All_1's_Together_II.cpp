959c2f2f04402420ba0649188db3172abb028334
Girdhari-patel <girdharip45@gmail.com>
2024-08-03 02:23:32 +0530

    imp

--- leetcode challenges/sliding window/2134.Minimum_Swaps_to_Group_All_1's_Together_II.cpp
+++ leetcode challenges/sliding window/2134.Minimum_Swaps_to_Group_All_1's_Together_II.cpp
@@ -38,4 +38,30 @@ Constraints:
 
 1 <= nums.length <= 105
 nums[i] is either 0 or 1.
+
+broCoders
 */
+
+class Solution {
+public:
+    int minSwaps(vector<int>& nums) {
+        int n = nums.size();
+        int count =0;
+        int ones = accumulate(nums.begin(), nums.end(), 0);
+    
+        nums.insert(nums.end(), nums.begin(), nums.end());
+        int ans = n;
+        int i=0, j=0, one=0;
+        while(j<2*n){
+            one+=(nums[j]==1);
+            
+            if(j-i+1==ones){
+                ans = min(ans, ones-one);
+                one -= (nums[i]==1);
+                i++;
+            }
+            j++;
+        }
+        return ans==n?0:ans;
+    }
+};
